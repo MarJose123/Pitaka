c84ea1e86e9ef6e209909023c5ad109ed98f71e3
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-26 18:47:41 +0800

    ♻️ refactor(tests, models, composer): Improve testing setup and fix code style issues.  Updated dependencies and addressed minor code style inconsistencies.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,11 +3,29 @@
 namespace MarJose123\Pitaka\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use MarJose123\Pitaka\PitakaServiceProvider;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    use LazilyRefreshDatabase;
+    use WithWorkbench;
+
+    /**
+     * Automatically enables package discoveries.
+     *
+     * @var bool
+     */
+    protected $enablesPackageDiscoveries = true;
+
+    /** {@inheritDoc} */
+    protected function shouldSeed()
+    {
+        return true;
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -27,7 +45,6 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
         /*
          foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/database/migrations') as $migration) {
             (include $migration->getRealPath())->up();
