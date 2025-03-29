a08c255d552e11761558a55f0a00271d1a6c8c9b
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-29 15:19:05 +0800

    ✅ test(`WalletDepositTest.php`): Add tests for wallet deposit functionality with decimal and integer values.  Includes tests using class return methods.

--- tests/Feature/WalletDepositTest.php
+++ tests/Feature/WalletDepositTest.php
@@ -20,6 +20,26 @@ test('it can deposit to a wallet in decimal', function () {
     assertDatabaseCount('wallets', 1);
     assertEquals($user->wallet('peso-wallet')->balance_float, 1000.58);
 });
+
+test('it can deposit to a wallet in decimal from class return', function () {
+    $user = \Workbench\App\Models\User::factory()->create();
+    $wallet = $user->wallets()->create([
+        'name' => 'Peso Wallet',
+        'slug' => 'peso-wallet',
+        'raw_balance' => 0,
+        'decimal_places' => 2,
+        'metadata' => [
+            'Currency' => 'PHP',
+        ],
+    ]);
+
+    $wallet->deposit(1000.58);
+
+    assertDatabaseCount('wallets', 1);
+    assertEquals($user->wallet('peso-wallet')->balance_float, 1000.58);
+    assertEquals($user->id, $wallet->owner->id);
+});
+
 test('it can deposit to a wallet in integer', function () {
     $user = \Workbench\App\Models\User::factory()->create();
     $wallet = $user->wallets()->create([
@@ -36,4 +56,22 @@ test('it can deposit to a wallet in integer', function () {
 
     assertDatabaseCount('wallets', 1);
     assertEquals($user->wallet('peso-wallet')->balance, 1000);
+});
+test('it can deposit to a wallet in integer from class return', function () {
+    $user = \Workbench\App\Models\User::factory()->create();
+    $wallet = $user->wallets()->create([
+        'name' => 'Peso Wallet',
+        'slug' => 'peso-wallet',
+        'raw_balance' => 0,
+        'decimal_places' => 2,
+        'metadata' => [
+            'Currency' => 'PHP',
+        ],
+    ]);
+
+    $wallet->deposit(1000);
+
+    assertDatabaseCount('wallets', 1);
+    assertEquals($user->wallet('peso-wallet')->balance, 1000);
+    assertEquals($user->id, $wallet->owner->id);
 });
